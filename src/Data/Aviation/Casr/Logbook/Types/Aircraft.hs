64ddbeaefd19ba138ca9d4c6a0bbf53c2b4bc623
Tony Morris <tmorris@tmorris.net>
2018-12-02 10:11:40 +1000

    .

---
+++ src/Data/Aviation/Casr/Logbook/Types/Aircraft.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Data.Aviation.Casr.Logbook.Types.Aircraft(
+  module A
+) where
+
+import Data.Aviation.Casr.Logbook.Types.Aircraft.AeroplaneDesignFeature as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.Aircraft as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.AircraftCategory as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.AircraftRegistration as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.AirshipDesignFeature as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.CASARegistration as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.DecDigit4 as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.GyroplaneDesignFeature as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.HelicopterDesignFeature as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.JetType as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.LandingGear as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.PropulsionPosition as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsions as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsions1 as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.PropulsionType as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.RAAusRegistration as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.RAAusRegistrationPrefix as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.RAAusRegistrationType as A
+import Data.Aviation.Casr.Logbook.Types.Aircraft.RPACategory as A
