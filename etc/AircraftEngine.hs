6bb0b492fa6ebcf01b6e47b08a9bd5fcd7162841
Tony Morris <tmorris@tmorris.net>
2018-12-29 22:17:59 +1000

    .

---
+++ etc/AircraftEngine.hs
@@ -0,0 +1,73 @@
+data PistonEngineConfiguration =
+  Parallel
+  | VConfiguration
+  | WConfiguration
+  | XConfiguration
+  | Opposed
+  | Radial
+
+data PistonEngineCycle =
+  FourStroke
+  | TwoStroke
+
+data PistonEngine =
+  PistonEngineConfiguration
+  PistonEngineCycle
+  Positive -- cylinders
+  Positive -- capacity
+
+data RotaryEngine =
+  Positive -- rotors
+  Positive -- capacity
+
+data InternalCombustionEngineAirInduction =
+  Supercharged
+  | Turbocharged
+  | SuperTurbocharged
+  | NaturalInduction
+
+data InternalCombustionEngineFuelInduction =
+  Carburetor
+  | FuelInjection
+
+data InternalCombustionEngineIgnition =
+  Diesel
+  | Spark
+
+data InternalCombustionEngineType =
+  _ PistonEngine
+  | _ RotaryEngine
+ 
+data InternalCombustionEngine =
+  InternalCombustionEngineAirInduction
+  InternalCombustionEngineFuelInduction
+  InternalCombustionEngineIgnition
+  InternalCombustionEngineType
+
+data JetType =
+  Turbojet
+  | Turbofan
+  | Turboprop
+  | Ramjet
+  | Scramjet
+
+data ElectricType =
+  BrushlessAC
+  | BrushedAC
+  | Brushless DC
+  | BrushedDC
+  | UniversalACDC
+  | SwitchedReluctance
+  | DirectDrive
+  | Linear
+
+data EngineType =
+  InternalCombustionEngine
+  | _ JetType
+  | Electric
+  | Rocket
+
+data Engine =
+  String -- manufacturer
+  String -- designation
+  EngineType
