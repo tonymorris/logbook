2d9265e0f5ed9bca004cf4bb5e54aef84244dddc
Tony Morris <tmorris@tmorris.net>
2019-01-04 10:38:03 +1000

    ttg

--- src/Data/Aviation/Casr/Logbook/Types/Aircraft/Propulsion/EngineType.hs
+++ src/Data/Aviation/Casr/Logbook/Types/Aircraft/Propulsion/EngineType.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DefaultSignatures #-}
 
 module Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion.EngineType where
 
@@ -8,15 +18,194 @@ import Control.Lens
 import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion.ElectricType
 import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion.InternalCombustionEngine
 import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion.JetType
+import Data.Void(Void, absurd)
 import GHC.Generics
 import Prelude
+import Util
 
-data EngineType =
-  InternalCombustionEngineEngineType InternalCombustionEngine
-  | Eletric ElectricType
-  | Jet JetType
-  | Rocket
-  deriving (Eq, Ord, Show, Generic)
+type family XInternalCombustionEngineEngineType x
+type family XElectric x
+type family XJet x
+type family XRocket x
+type family XEngineType x
 
-makeClassy ''EngineType
-makeClassyPrisms ''EngineType
+data EngineType_ x =
+  InternalCombustionEngineEngineType_ !(XInternalCombustionEngineEngineType x) InternalCombustionEngine
+  | Electric_ !(XElectric x) ElectricType
+  | Jet_ !(XJet x) JetType
+  | Rocket_ !(XRocket x)
+  | EngineType_ !(XEngineType x)
+  deriving Generic
+
+deriving instance (Eq (XInternalCombustionEngineEngineType x), Eq (XElectric x), Eq (XJet x), Eq (XRocket x), Eq (XEngineType x)) =>
+  Eq (EngineType_ x)
+
+deriving instance (Ord (XInternalCombustionEngineEngineType x), Ord (XElectric x), Ord (XJet x), Ord (XRocket x), Ord (XEngineType x)) =>
+  Ord (EngineType_ x)
+
+deriving instance (Show (XInternalCombustionEngineEngineType x), Show (XElectric x), Show (XJet x), Show (XRocket x), Show (XEngineType x)) =>
+  Show (EngineType_ x)
+
+class HasEngineType a e | a -> e where
+  engineType ::
+    Lens' a (EngineType_ e)
+  xEngineType ::
+    (
+      XInternalCombustionEngineEngineType e ~ x
+    , XElectric e ~ x
+    , XRocket e ~ x
+    , XJet e ~ x
+    , XEngineType e ~ Void
+    ) =>
+    Lens' a x
+  default xEngineType ::
+    (
+      XInternalCombustionEngineEngineType () ~ x
+    , XElectric () ~ x
+    , XRocket () ~ x
+    , XJet () ~ x
+    , XEngineType e ~ Void
+    ) =>
+    Lens' a x
+  xEngineType f a =
+    fmap (\() -> a) (f ())
+
+instance HasEngineType (EngineType_ e) e where
+  engineType =
+    id
+  xEngineType f (InternalCombustionEngineEngineType_ x t) =
+    fmap (\x' -> InternalCombustionEngineEngineType_ x' t) (f x)
+  xEngineType f (Electric_ x t) =
+    fmap (\x' -> Electric_ x' t) (f x)
+  xEngineType f (Rocket_ x) =
+    fmap (\x' -> Rocket_ x') (f x)
+  xEngineType f (Jet_ x t) =
+    fmap (\x' -> Jet_ x' t) (f x)
+  xEngineType _ (EngineType_ x) =
+    absurd x
+
+class AsEngineType a e | a -> e where
+  _EngineType ::
+    Prism' a (EngineType_ e)
+  _InternalCombustionEngineEngineType ::
+    Prism' a (XInternalCombustionEngineEngineType e, InternalCombustionEngine)
+  _InternalCombustionEngineEngineType' ::
+    XInternalCombustionEngineEngineType e ~ () =>
+    Prism' a InternalCombustionEngine
+  _InternalCombustionEngineEngineType' =
+    _InternalCombustionEngineEngineType . unproduct
+  _Electric ::
+    Prism' a (XElectric e, ElectricType)
+  _Electric' ::
+    XElectric e ~ () =>
+    Prism' a ElectricType
+  _Electric' =
+    _Electric . unproduct
+  _Rocket ::
+    Prism' a (XRocket e)
+  _Jet ::
+    Prism' a (XJet e, JetType)
+  _Jet' ::
+    XJet e ~ () =>
+    Prism' a JetType
+  _Jet' =
+    _Jet . unproduct
+  _XEngineType ::
+    Prism' a (XEngineType e)
+    
+instance AsEngineType (EngineType_ e) e where
+  _EngineType =
+    id
+  _InternalCombustionEngineEngineType =
+    prism'
+      (\(x, t) -> InternalCombustionEngineEngineType_ x t)
+      (
+        \case
+          InternalCombustionEngineEngineType_ x t ->
+            Just (x, t)
+          _ ->
+            Nothing
+      )
+  _Electric =
+    prism'
+      (\(x, t) -> Electric_ x t)
+      (
+        \case
+          Electric_ x t ->
+            Just (x, t)
+          _ ->
+            Nothing
+      )
+  _Rocket =
+    prism'
+      Rocket_
+      (
+        \case
+          Rocket_ x ->
+            Just x
+          _ ->
+            Nothing
+      )
+  _Jet =
+    prism'
+      (\(x, t) -> Jet_ x t)
+      (
+        \case
+          Jet_ x t ->
+            Just (x, t)
+          _ ->
+            Nothing
+      )
+  _XEngineType =
+    prism'
+      EngineType_
+      (
+        \case
+          EngineType_ x ->
+            Just x
+          _ ->
+            Nothing
+      )
+
+type EngineType =
+  EngineType_ ()
+
+type instance XInternalCombustionEngineEngineType () =
+  ()
+type instance XElectric () =
+  ()
+type instance XRocket () =
+  ()
+type instance XJet () =
+  ()
+type instance XEngineType () =
+  Void
+
+pattern InternalCombustionEngineEngineType ::
+  InternalCombustionEngine
+  -> EngineType
+pattern InternalCombustionEngineEngineType t <- InternalCombustionEngineEngineType_ _ t
+  where InternalCombustionEngineEngineType t = InternalCombustionEngineEngineType_ () t
+
+pattern Electric ::
+  ElectricType
+  -> EngineType
+pattern Electric t <- Electric_ _ t
+  where Electric t = Electric_ () t
+
+pattern Rocket ::
+  EngineType
+pattern Rocket <- Rocket_ _
+  where Rocket = Rocket_ ()
+
+pattern Jet ::
+  JetType
+  -> EngineType
+pattern Jet t <- Jet_ _ t
+  where Jet t = Jet_ () t
+
+pattern EngineType ::
+  Void
+  -> EngineType
+pattern EngineType v <- EngineType_ v
+  where EngineType v = EngineType_ v
--- src/Data/Aviation/Casr/Logbook/Types/Aircraft/Propulsion/InternalCombustionEngineType.hs
+++ src/Data/Aviation/Casr/Logbook/Types/Aircraft/Propulsion/InternalCombustionEngineType.hs
@@ -1,19 +1,154 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DefaultSignatures #-}
 
 module Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion.InternalCombustionEngineType where
 
-import Control.Lens
+import Control.Lens(Lens', Prism', prism')
 import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion.PistonEngine
 import Data.Aviation.Casr.Logbook.Types.Aircraft.Propulsion.RotaryEngine
-import GHC.Generics
+import Data.Void(Void, absurd)
+import GHC.Generics(Generic)
 import Prelude
+import Util
 
-data InternalCombustionEngineType =
-  PistonEngineType PistonEngine
-  | RotaryEngineType RotaryEngine
-  deriving (Eq, Ord, Show, Generic)
+type family XPistonEngineType x
+type family XRotaryEngineType x
+type family XInternalCombustionEngineType x
 
-makeClassy ''InternalCombustionEngineType
-makeClassyPrisms ''InternalCombustionEngineType
+data InternalCombustionEngineType_ x=
+  PistonEngineType_ !(XPistonEngineType x) PistonEngine
+  | RotaryEngineType_ !(XRotaryEngineType x) RotaryEngine
+  | InternalCombustionEngineType_ !(XInternalCombustionEngineType x)
+  deriving Generic
+
+deriving instance (Eq (XPistonEngineType x), Eq (XRotaryEngineType x), Eq (XInternalCombustionEngineType x)) =>
+  Eq (InternalCombustionEngineType_ x)
+
+deriving instance (Ord (XPistonEngineType x), Ord (XRotaryEngineType x), Ord (XInternalCombustionEngineType x)) =>
+  Ord (InternalCombustionEngineType_ x)
+
+deriving instance (Show (XPistonEngineType x), Show (XRotaryEngineType x), Show (XInternalCombustionEngineType x)) =>
+  Show (InternalCombustionEngineType_ x)
+
+class HasInternalCombustionEngineType a e | a -> e where
+  internalCombustionEngineType ::
+    Lens' a (InternalCombustionEngineType_ e)
+  xInternalCombustionEngineType ::
+    (
+      XPistonEngineType e ~ x
+    , XRotaryEngineType e ~ x
+    , XInternalCombustionEngineType e ~ Void
+    ) =>
+    Lens' a x
+  default xInternalCombustionEngineType ::
+    (
+      XPistonEngineType () ~ x
+    , XRotaryEngineType () ~ x
+    , XInternalCombustionEngineType e ~ Void
+    ) =>
+    Lens' a x
+  xInternalCombustionEngineType f a =
+    fmap (\() -> a) (f ())
+
+instance HasInternalCombustionEngineType (InternalCombustionEngineType_ e) e where
+  internalCombustionEngineType =
+    id
+  xInternalCombustionEngineType f (PistonEngineType_ x t) =
+    fmap (\x' -> PistonEngineType_ x' t) (f x)
+  xInternalCombustionEngineType f (RotaryEngineType_ x t) =
+    fmap (\x' -> RotaryEngineType_ x' t) (f x)
+  xInternalCombustionEngineType _ (InternalCombustionEngineType_ x) =
+    absurd x
+
+class AsInternalCombustionEngineType a e | a -> e where
+  _InternalCombustionEngineType ::
+    Prism' a (InternalCombustionEngineType_ e)
+  _PistonEngineType ::
+    Prism' a (XPistonEngineType e, PistonEngine)
+  _PistonEngineType' ::
+    XPistonEngineType e ~ () =>
+    Prism' a PistonEngine
+  _PistonEngineType' =
+    _PistonEngineType . unproduct
+  _RotaryEngineType ::
+    Prism' a (XRotaryEngineType e, RotaryEngine)
+  _RotaryEngineType' ::
+    XRotaryEngineType e ~ () =>
+    Prism' a RotaryEngine
+  _RotaryEngineType' =
+    _RotaryEngineType . unproduct
+  _XInternalCombustionEngineType ::
+    Prism' a (XInternalCombustionEngineType e)
+
+instance AsInternalCombustionEngineType (InternalCombustionEngineType_ e) e where
+  _InternalCombustionEngineType =
+    id
+  _PistonEngineType =
+    prism'
+      (\(x, t) -> PistonEngineType_ x t)
+      (
+        \case
+          PistonEngineType_ x t ->
+            Just (x, t)
+          _ ->
+            Nothing
+      )
+  _RotaryEngineType =
+    prism'
+      (\(x, t) -> RotaryEngineType_ x t)
+      (
+        \case
+          RotaryEngineType_ x t ->
+            Just (x, t)
+          _ ->
+            Nothing
+      )
+  _XInternalCombustionEngineType =
+    prism'
+      InternalCombustionEngineType_
+      (
+        \case
+          InternalCombustionEngineType_ x ->
+            Just x
+          _ ->
+            Nothing
+      )
+
+type InternalCombustionEngineType =
+  InternalCombustionEngineType_ ()
+
+type instance XPistonEngineType () =
+  ()
+type instance XRotaryEngineType () =
+  ()
+type instance XInternalCombustionEngineType () =
+  Void
+
+pattern PistonEngineType ::
+  PistonEngine
+  -> InternalCombustionEngineType
+pattern PistonEngineType t <- PistonEngineType_ _ t
+  where PistonEngineType t = PistonEngineType_ () t
+
+pattern RotaryEngineType ::
+  RotaryEngine
+  -> InternalCombustionEngineType
+pattern RotaryEngineType t <- RotaryEngineType_ _ t
+  where RotaryEngineType t = RotaryEngineType_ () t
+
+pattern InternalCombustionEngineType ::
+  Void
+  -> InternalCombustionEngineType
+pattern InternalCombustionEngineType v <- InternalCombustionEngineType_ v
+  where InternalCombustionEngineType v = InternalCombustionEngineType_ v
